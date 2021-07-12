0d31226da8c7b9a63101f7a782ad1bb1e9ec5cc6
Atul-Verma-Git <84612253+Atul-Verma-Git@users.noreply.github.com>
2021-07-12 15:10:23 +0530

    Time: 128 ms (37.86%), Space: 21.7 MB (38.89%) - LeetHub

---
+++ gray-code/gray-code.py
@@ -0,0 +1,17 @@
+'''
+n = 1 0, 1
+n =2 -> 00 01 11 10
+n = 3 -> 110 111 101 100
+n = 4 -> 
+'''
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
+        output = [0,1]
+        for _ in range(1,n):
+            b = len(output)
+            for __ in reversed(range(b)):
+                output.append(2**(_) + output[__])
+        return output
+            
+        
+        
