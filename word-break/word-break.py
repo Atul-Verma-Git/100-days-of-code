1dc710b42edb7cd0975fa4a87d7389ba9f486614
Atul-Verma-Git <84612253+Atul-Verma-Git@users.noreply.github.com>
2021-07-12 22:19:13 +0530

    Time: 44 ms (39.95%), Space: 14.5 MB (47.35%) - LeetHub

--- word-break/word-break.py
+++ word-break/word-break.py
@@ -7,10 +7,10 @@ else
 '''
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        ok = [True]
+        dp = [True]
         for i in range(1, len(s)+1):
-            ok += any(ok[j] and s[j:i] in wordDict for j in range(i)),
-        return ok[-1]
+            dp += any(dp[j] and s[j:i] in wordDict for j in range(i)),
+        return dp[-1]
                                         
             
             
